0b1ce80c4d9db38ebeea0a532e536f32c615fc0e
yazidmaaouni <yazidmaaouni@gmail.comC^git config --global user.email yazidmaaouni@gmail.com>
2023-10-12 07:47:13 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - Multiples two integers
+ * @x: The first number to multiply
+ * @y: The second number to multiply with the first number
+ * Return: The result of the multiplication between x param and y param
+ */
+
+int mul(int x , int y)
+{
+
+	return (x * y);
+
+}
