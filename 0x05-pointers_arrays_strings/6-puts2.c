3736230d7f8ae999e4c079f63ba805d40e7f9eae
yazidmaaouni <yazidmaaouni@gmail.com>
2023-10-16 12:59:05 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints every other chracter of a string
+ * @s: The string to be treated
+ * Return:void
+ */
+
+void _puts2(char *str)
+{
+	int i;
+	int j = 0;
+
+	while (str[j] != '\0')
+	{
+	j++;
+	}
+
+	for(i = 0; i < j; i += 2)
+	{
+	_putchar(str[i]);
+	}
+	_putchar('\n');
+}
