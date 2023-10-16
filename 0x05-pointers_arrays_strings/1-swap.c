193a12ad002851685b28d2e1bd03a1da3a725161
yazidmaaouni <yazidmaaouni@gmail.comC^git config --global user.email yazidmaaouni@gmail.com>
2023-10-15 21:40:27 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integer
+ * @a: integer to swap
+ * @b: integer to swap
+ */
+
+void swap_int(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+
+}
