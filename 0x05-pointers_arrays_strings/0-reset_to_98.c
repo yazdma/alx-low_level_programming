5b50512e2a8200053c8ecb1a9ca2184a3b605d84
yazidmaaouni <yazidmaaouni@gmail.comC^git config --global user.email yazidmaaouni@gmail.com>
2023-10-15 21:30:57 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - a function that takes a pointer to an int as parameter
+ * and updates the value it point to to 98
+ * @n: input
+ * Return: n
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
