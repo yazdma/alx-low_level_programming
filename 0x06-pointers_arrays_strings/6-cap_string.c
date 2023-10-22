1e2b0422490b1ef176de33da926499b1e3f60389
yazidmaaouni <yazidmaaouni@gmail.com>
2023-10-22 07:54:29 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,24 +12,27 @@ char *cap_string(char *str)
 
 	while (str[index])
 	{
-	while (!(str[index] >= 'a' && str[index] <= 'z'))
-	index++;
-	if (str[index - 1] == ' ' ||
-	 str[index - 1] == '\t' ||
-         str[index - 1] == '\n' ||
-	 str[index - 1] == ','  ||
-	 str[index - 1] == ';'  ||
-	 str[index - 1] == '.'  ||
-	 str[index - 1] == '!'  ||
-	 str[index - 1] == '?'  ||
-	 str[index - 1] == '"'  ||
-	 str[index - 1] == '('  ||
-	 str[index - 1] == ')'  ||
-	 str[index - 1] == '{'  ||
-	 str[index - 1] == '}'  ||
-	 index == 0)
-	 str[index] -= 32;
-	 index++;
-	 }
-	 return (str);
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+	               
+			index++;
+
+		if (str[index - 1] == ' ' ||
+                str[index - 1] == '\t'  ||
+                str[index - 1] == '\n'  ||
+		str[index - 1] == ','  ||
+		str[index - 1] == ';'  ||
+		str[index - 1] == '.'  ||
+		str[index - 1] == '!'  ||
+		str[index - 1] == '?'  ||
+		str[index - 1] == '"'  ||
+		str[index - 1] == '('  ||
+		str[index - 1] == ')'  ||
+		str[index - 1] == '{'  ||
+		str[index - 1] == '}'  ||
+		index == 0)
+			str[index] -= 32;
+		index++;
+	}
+	return (str);
 }
+
