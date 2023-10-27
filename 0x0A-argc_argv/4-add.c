eb550cf50ade966588b1ea7e706a1a526e733dd7
yazidmaaouni <yazidmaaouni@gmail.com>
2023-10-27 12:24:16 -0700

    100-change.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -42,7 +42,7 @@ int main(int argc, char *argv[])
 	int sum = 0;
 
 	count = 1;
-	while ( count < argc) /*Goes through the whole array*/
+	while (count < argc) /*Goes through the whole array*/
 	{
         if (check_num(argv[count]))
 	{
