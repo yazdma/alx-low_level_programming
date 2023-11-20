32fba4344eeb76e9ecddce995dcf68d9e8a6950f
yazidmaaouni <yazidmaaouni@gmail.com>
2023-11-20 08:14:37 -0800

    3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,5 +1,6 @@
 #include "lists.h"
-
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * add_nodeint_end - Adds a new node at the end of a listint_t list.
  * @head: A pointer to the address of the head of a listint_t list.
