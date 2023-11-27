c1b9d42d35a124b8961b82a0bf86aea9b19fb3be
yazidmaaouni <yazidmaaouni@gmail.com>
2023-11-27 12:53:09 -0800

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,7 +3,7 @@
 /**
  * create_file - prog creates a file
  * @filename: filename
- * @textcontent: content writed in the file
+ * @text_content: content writed in the file
  *
  * Return: 1 if it success. -1 if it fails.
  */
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (!text_content)
-		 text_content = "";
+		text_content = "";
 
 	for (nletters = 0; text_content[nletters]; nletters++)
 		;
